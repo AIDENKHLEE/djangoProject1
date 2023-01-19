5ddfa2db404bf6d179bd68398cc20d7d5a3bed85
Kyunghyun Lee <kyunghyun.lee7@gmail.com>
2023-01-19 13:40:28 -0500

    module 5

--- myapp/views.py
+++ myapp/views.py
@@ -1,4 +1,7 @@
 from django.shortcuts import render
+from myapp import support_functions
+from myapp.models import Currency
+
 
 # Create your views here.
 
@@ -7,5 +10,33 @@ def home(request):
     import datetime
     time = datetime.datetime.now()
     data["time_of_day"] = time
-    print(time)
     return render(request,"home.html", context=data)
+
+from django.http import HttpResponseRedirect
+from django.urls import reverse
+def maintenance(request):
+    data = dict()
+    try:
+        choice = request.GET['selection']
+        if choice == "currencies":
+            support_functions.add_currencies(support_functions.get_currency_list())
+            c_list = Currency.objects.all()
+            print("Got c_list",len(c_list))
+            data['currencies'] = c_list
+            return HttpResponseRedirect(reverse('currencies'))
+    except:
+        pass
+        return render(request,"maintenance.html",context=data)
+
+def view_currencies(request):
+    data = dict()
+    c_list = Currency.objects.all()
+    data['currencies'] = c_list
+    return render(request,'currencies.html',context=data)
+
+
+def currency_selection(request):
+    data = dict()
+    currencies =Currency.objects.all()
+    data['currencies'] = currencies
+    return render(request,"currency_selector.html",data)
